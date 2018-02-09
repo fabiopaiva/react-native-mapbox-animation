65d6b880d1ff37aa2e60c97b50e952e2b487f6f6
Fábio Paiva <fabio@paiva.info>
2018-02-09 01:11:48 +0100

    Add Mapbox and Draw a line

--- App.js
+++ App.js
@@ -6,53 +6,104 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
+  View,
   Text,
-  View
 } from 'react-native';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
+import { lineString as makeLineString } from '@turf/helpers';
+import { MAPBOX_ACCESS_TOKEN } from './MapboxAccessToken';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
+
+const layerStyles = MapboxGL.StyleSheet.create({
+  route: {
+    lineColor: '#007aff',
+    lineWidth: 5,
+    lineOpacity: 0.85,
+    lineJoin: MapboxGL.LineJoin.Round,
+    lineCap: MapboxGL.LineCap.Round,
   },
 });
+
+const origin = 'Av. do Contorno, 6061 - São Pedro, Belo Horizonte - MG, 30110-929, Brazil';
+const destination = 'Av. Augusto de Lima, 744 - Centro, Belo Horizonte - MG, 30190-922, Brazil';
+
+
+export default class App extends Component {
+  state = {
+    centerCoordinate: null,
+    error: null,
+    route: null,
+  }
+
+  _mapRef;
+
+  componentWillMount() {
+   this.getDirections();
+  }
+
+  async getDirections() {
+    try {
+      const response = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}`)
+      const data = await response.json();
+
+      if (data.status === 'OK') {
+        if (data.routes && data.routes.length && data.routes[0].legs.length) {
+          const route = data.routes[0];
+          if (route.legs.length) {
+            const leg = route.legs[0];
+            const centerCoordinate = [leg.start_location.lng, leg.start_location.lat];
+            const coordinates = leg.steps.reduce((acc, step) => {
+              acc.push([step.start_location.lng, step.start_location.lat]);
+              acc.push([step.end_location.lng, step.end_location.lat]);
+              return acc;
+            }, []);
+
+            this.setState({ centerCoordinate, route: makeLineString(coordinates) });
+
+            if (this._mapRef) {
+              this._mapRef.fitBounds(
+                [leg.start_location.lng, leg.start_location.lat],
+                [leg.end_location.lng, leg.end_location.lat]
+              );
+            }
+          }
+        }
+      }
+    }
+    catch(error) {
+      this.setState({ error: error.toString() + JSON.stringify(error) })
+    }
+  }
+
+  render() {
+    const { centerCoordinate, error, route } = this.state;
+    if (error) return <Text>{error}</Text>
+
+    return (
+      <View style={styles.container}>
+        <MapboxGL.MapView
+          ref={(ref) => { this._mapRef = ref }}
+          style={styles.map}
+          centerCoordinate={centerCoordinate}
+        >
+        {route && (
+          <MapboxGL.ShapeSource id='routeSource' shape={route}>
+            <MapboxGL.LineLayer id='routeFill' style={layerStyles.route} />
+          </MapboxGL.ShapeSource>
+        )}
+        </MapboxGL.MapView>
+      </View>
+    );
+  }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Set Mapbox Token
+
+Create your access token on (Mapbox)[https://github.com/mapbox/react-native-mapbox-gl#sign-up-for-mapbox]
+
+Create the file `MapboxAccessToken.js` with your token from .dist file or:
+
+    echo "export const MAPBOX_ACCESS_TOKEN = 'your-token-here'" >> MapboxAccessToken.js
+
+# Run the project
+
+  yarn start
